36a1e397e17620beb8afb0ef6c1efc0f3637de48
spring security <licherep803@gmail.com>
2021-12-16 11:16:10 +0300

    add ribbon

--- src/main/java/tech/pragmat/countryregionservice/web/controller/ConnectionWithGeoLite2CountryController.java
+++ src/main/java/tech/pragmat/countryregionservice/web/controller/ConnectionWithGeoLite2CountryController.java
@@ -1,10 +1,12 @@
 package tech.pragmat.countryregionservice.web.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 import tech.pragmat.countryregionservice.service.CountyRegionFeignService;
 
 @Slf4j
@@ -12,16 +14,24 @@ import tech.pragmat.countryregionservice.service.CountyRegionFeignService;
 @RequestMapping("/connectGeo2LiteService")
 public class ConnectionWithGeoLite2CountryController {
 
+    @Value("${cr.url}")
+    String a;
+
     private final CountyRegionFeignService countyRegionFeignService;
 
-    public ConnectionWithGeoLite2CountryController(CountyRegionFeignService countyRegionFeignService) {
+    private final RestTemplate restTemplate;
+
+    public ConnectionWithGeoLite2CountryController(CountyRegionFeignService countyRegionFeignService, RestTemplate restTemplate) {
         this.countyRegionFeignService = countyRegionFeignService;
+        this.restTemplate = restTemplate;
     }
 
     @GetMapping("/getRegionByIp")
     public String get(@RequestParam String ip) {
+
         log.info("try get region by ip");
-        return countyRegionFeignService.getRegionName(ip);
+//        return countyRegionFeignService.getRegionName(ip);
+        return restTemplate.getForObject(a + ip, String.class);
 
     }
 }
