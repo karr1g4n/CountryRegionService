a72249f175b416ef3d5859d24a9bcd4157193760
Karr1g4n <licherep803@gmail.com>
2022-02-01 18:00:40 +0200

    sql script

---
+++ src/main/resources/db/migration/V1__create_table.sql
@@ -0,0 +1,16 @@
+create table country_region_access
+(
+    id     integer not null primary key,
+    access varchar(255)
+
+);
+create table country_region
+(
+    id                integer not null primary key,
+    country           varchar(255),
+    region            varchar(255),
+    country_access_id integer,
+    region_access_id  integer,
+    FOREIGN KEY (country_access_id) REFERENCES country_region_access (id),
+    FOREIGN KEY (region_access_id) REFERENCES country_region_access (id)
+)
