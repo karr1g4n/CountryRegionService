9a6d2afa95d9cfbf1ca7fd51464861b6952935ba
Karr1g4n <licherep803@gmail.com>
2022-02-09 17:03:17 +0200

    created new testes with JUnit and one test with mockito

--- src/test/java/tech/pragmat/countryregionservice/CountryRegionApplicationTests.java
+++ src/test/java/tech/pragmat/countryregionservice/CountryRegionApplicationTests.java
@@ -10,7 +10,11 @@ import tech.pragmat.countryregionservice.model.entity.CountryRegion;
 import tech.pragmat.countryregionservice.model.entity.CountryRegionAccess;
 import tech.pragmat.countryregionservice.repository.CountryRegionRepository;
 import tech.pragmat.countryregionservice.service.CountryRegionService;
+import tech.pragmat.countryregionservice.web.controller.ConnectionWithGeoLite2CountryController;
 import tech.pragmat.countryregionservice.web.controller.CountryRegionController;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 class CountryRegionApplicationTests {
@@ -23,6 +27,8 @@ class CountryRegionApplicationTests {
 
     @Autowired
     private CountryRegionController countryRegionController;
+
+    private ConnectionWithGeoLite2CountryController connectionWithGeoLite2CountryController;
 
     private final CountryRegionAccess countryRegionAccess = new CountryRegionAccess(1, "access");
 
@@ -72,29 +78,18 @@ class CountryRegionApplicationTests {
         Assertions.assertEquals(countryRegion, countryRegionService.getCountryRegionByName("Andorra"));
     }
 
-    //    @Test
-    //    @Transactional
-    //    public void TestDeleteByName() {
-    //        CountryRegion countryRegion = new CountryRegion(1, "Andorra", "world", countryRegionAccess, countryRegionAccess);
-    //        countryRegionController.delCountryByName("Andorra");
-    ////        System.out.println(countryRegionService.getCountryRegionByName("Andorra"));
-    //        //        Assertions.assertEquals(countryRegion, countryRegionService.getCountryRegionByName("Andorra"));
-    //    }
-
-//    @Test
-//    @Transactional
-//    public void Test() {
-//        CountryRegion countryRegion = countryRegionService.getCountryRegionByName("Andorra");
-//        countryRegionRepository.delete(countryRegion);
-//        Assert.assertEquals(null, countryRegionService.getCountryRegionByName("Andorra"));
-//    }
-
-
     @Test
     @Transactional
-    public void Test() {
-        CountryRegion countryRegion = countryRegionService.getCountryRegionByName("Andorra");
-        countryRegionRepository.deleteByCountry("Andorra");
-        Assert.assertEquals(null, countryRegionService.getCountryRegionByName("Andorra"));
+    public void TestDeleteByName() {
+        countryRegionController.delCountryByName("Andorra");
+        Assertions.assertNull(countryRegionRepository.findByCountry("Andorra"));
     }
+
+    @Test
+    public void Test() {
+        ConnectionWithGeoLite2CountryController mockConnection = mock(ConnectionWithGeoLite2CountryController.class);
+        when(mockConnection.get("103.13.64.0")).thenReturn("Afghanistan");
+        Assert.assertEquals(mockConnection.get("103.13.64.1"), "Afghanistan");
+    }
+
 }
