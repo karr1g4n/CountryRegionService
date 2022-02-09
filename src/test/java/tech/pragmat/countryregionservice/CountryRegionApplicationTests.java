8a6f87563eede1313075ef5f03985c29373365b7
Karr1g4n <licherep803@gmail.com>
2022-02-09 12:21:19 +0200

    created new test

--- src/test/java/tech/pragmat/countryregionservice/CountryRegionApplicationTests.java
+++ src/test/java/tech/pragmat/countryregionservice/CountryRegionApplicationTests.java
@@ -8,6 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 import tech.pragmat.countryregionservice.model.entity.CountryRegion;
 import tech.pragmat.countryregionservice.model.entity.CountryRegionAccess;
+import tech.pragmat.countryregionservice.repository.CountryRegionRepository;
 import tech.pragmat.countryregionservice.service.CountryRegionService;
 import tech.pragmat.countryregionservice.web.controller.CountryRegionController;
 
@@ -15,10 +16,13 @@ import tech.pragmat.countryregionservice.web.controller.CountryRegionController;
 class CountryRegionApplicationTests {
 
     @Autowired
-    private CountryRegionController countryRegionController;
+    private CountryRegionRepository countryRegionRepository;
 
     @Autowired
     private CountryRegionService countryRegionService;
+
+    @Autowired
+    private CountryRegionController countryRegionController;
 
     private final CountryRegionAccess countryRegionAccess = new CountryRegionAccess(1, "access");
 
@@ -68,12 +72,29 @@ class CountryRegionApplicationTests {
         Assertions.assertEquals(countryRegion, countryRegionService.getCountryRegionByName("Andorra"));
     }
 
+    //    @Test
+    //    @Transactional
+    //    public void TestDeleteByName() {
+    //        CountryRegion countryRegion = new CountryRegion(1, "Andorra", "world", countryRegionAccess, countryRegionAccess);
+    //        countryRegionController.delCountryByName("Andorra");
+    ////        System.out.println(countryRegionService.getCountryRegionByName("Andorra"));
+    //        //        Assertions.assertEquals(countryRegion, countryRegionService.getCountryRegionByName("Andorra"));
+    //    }
+
+//    @Test
+//    @Transactional
+//    public void Test() {
+//        CountryRegion countryRegion = countryRegionService.getCountryRegionByName("Andorra");
+//        countryRegionRepository.delete(countryRegion);
+//        Assert.assertEquals(null, countryRegionService.getCountryRegionByName("Andorra"));
+//    }
+
+
     @Test
     @Transactional
-    public void TestDeleteByName() {
-        CountryRegion countryRegion = new CountryRegion(1, "Andorra", "world", countryRegionAccess, countryRegionAccess);
-        countryRegionController.delCountryByName("Andorra");
-//        System.out.println(countryRegionService.getCountryRegionByName("Andorra"));
-        //        Assertions.assertEquals(countryRegion, countryRegionService.getCountryRegionByName("Andorra"));
+    public void Test() {
+        CountryRegion countryRegion = countryRegionService.getCountryRegionByName("Andorra");
+        countryRegionRepository.deleteByCountry("Andorra");
+        Assert.assertEquals(null, countryRegionService.getCountryRegionByName("Andorra"));
     }
 }
