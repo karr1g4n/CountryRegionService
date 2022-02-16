592e41c4861c40bf2fc2cb8c181d808712095146
spring security <licherep803@gmail.com>
2022-02-16 22:28:40 +0900

    created new model for new table

---
+++ src/main/java/tech/pragmat/countryregionservice/model/entity/Country.java
@@ -0,0 +1,29 @@
+package tech.pragmat.countryregionservice.model.entity;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@SuppressFBWarnings("EI_EXPOSE_REP")
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+public class Country {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String country;
+
+}
