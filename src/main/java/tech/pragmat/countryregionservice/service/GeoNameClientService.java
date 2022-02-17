18d50ecddfd8a3a22631791297d8fd9845d94a05
Karr1g4n <licherep803@gmail.com>
2022-02-17 15:20:41 +0200

    added try catch construction

--- src/main/java/tech/pragmat/countryregionservice/service/GeoNameClientService.java
+++ src/main/java/tech/pragmat/countryregionservice/service/GeoNameClientService.java
@@ -1,5 +1,6 @@
 package tech.pragmat.countryregionservice.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
@@ -11,6 +12,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Service
+@Slf4j
 public class GeoNameClientService {
 
     private final GeoNameClient geoNameClient;
@@ -19,21 +21,27 @@ public class GeoNameClientService {
         this.geoNameClient = geoNameClient;
     }
 
-    public List<String> getAllCountries() throws IOException {
+    public List<String> getAllCountries(){
         List<String> countryNames = new ArrayList<>();
-        StringReader stringReader = new StringReader(geoNameClient.getAllCountryInfo());
-        Iterable<CSVRecord> names = CSVFormat.RFC4180.withDelimiter('\t').parse(stringReader);
+        try {
+            StringReader stringReader = new StringReader(geoNameClient.getAllCountryInfo());
+            Iterable<CSVRecord> names = CSVFormat.RFC4180.withDelimiter('\t').parse(stringReader);
 
-        for (CSVRecord name : names) {
-            String countryName = "";
-            if (name.size() > 4 && !"#".equals(name.get(0)) && !name.get(4).equals("Country")) {
+            for (CSVRecord name : names) {
+                String countryName = "";
+                if (name.size() > 4 && !"#".equals(name.get(0)) && !name.get(4).equals("Country")) {
 
-                countryName = name.get(4);
+                    countryName = name.get(4);
+                }
+                if (!countryName.isEmpty()) {
+                    countryNames.add(countryName);
+                }
             }
-            if (!countryName.isEmpty()) {
-                countryNames.add(countryName);
-            }
+            return countryNames;
+        } catch (Exception e) {
+            log.error(String.valueOf(e));
         }
         return countryNames;
+
     }
 }
