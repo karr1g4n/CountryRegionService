61111f8a9b27877868a1b565aa1f491d425050e6
Karr1g4n <licherep803@gmail.com>
2022-02-22 15:45:53 +0200

    created controller for Country

---
+++ src/main/java/tech/pragmat/countryregionservice/web/controller/CountryController.java
@@ -0,0 +1,49 @@
+package tech.pragmat.countryregionservice.web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import tech.pragmat.countryregionservice.model.entity.Country;
+import tech.pragmat.countryregionservice.service.CountryService;
+
+import java.util.List;
+
+@RestController
+public class CountryController {
+
+    private final CountryService countryService;
+
+    @Autowired
+    public CountryController(CountryService countryService) {
+        this.countryService = countryService;
+    }
+
+    @PostMapping()
+    public Country addCountryByName(@RequestParam String name) {
+        return countryService.addCountry(name);
+    }
+
+    @PostMapping("/add/All/Countries")
+    public void addAllCountries() {
+        countryService.addAllCountry();
+    }
+
+    @GetMapping("get/All/Countries")
+    public List<Country> getAllCountries() {
+        return countryService.getAllCountry();
+    }
+
+    @GetMapping
+    public Country getCountryByName (@RequestParam String name){
+        return countryService.getCountryByName(name);
+    }
+
+    @DeleteMapping()
+    public void deleteCountryByName(@RequestParam String name){
+        countryService.deleteCountryByName(name);
+    }
+
+}
