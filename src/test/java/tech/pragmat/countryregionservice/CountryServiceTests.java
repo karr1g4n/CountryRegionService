34824f9e34066656be27943639bd5ae05d25190a
Karr1g4n <licherep803@gmail.com>
2022-02-22 15:45:11 +0200

    created tests for CountryService

--- src/test/java/tech/pragmat/countryregionservice/CountryServiceTests.java
+++ src/test/java/tech/pragmat/countryregionservice/CountryServiceTests.java
@@ -1,119 +1,111 @@
 package tech.pragmat.countryregionservice;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.util.TestPropertyValues;
-import org.springframework.context.ApplicationContextInitializer;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import tech.pragmat.countryregionservice.model.entity.Country;
 import tech.pragmat.countryregionservice.repository.CountryRepository;
-import tech.pragmat.countryregionservice.service.CountryNameClientService;
 import tech.pragmat.countryregionservice.service.CountryService;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 @SpringBootTest
 @Testcontainers
-@ContextConfiguration
 public class CountryServiceTests {
 
+    @Autowired
+    private CountryService countryService;
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+
+
     private static final String DOCKER_IMAGE = "postgres:13";
-    private static final String DB_NAME = "country_region";
+    private static final String DB_NAME = "country";
     private static final String USERNAME = "postgres";
     private static final String PASSWORD = "43ahegib";
 
-    @Autowired
-    public CountryService countryService;
-
-    @Autowired
-    public CountryRepository countryRepository;
-
-    @Autowired
-    public CountryNameClientService countryNameClientService;
-
     @Container
-    public static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>(DOCKER_IMAGE)
-            .withDatabaseName(DB_NAME)
+    public static PostgreSQLContainer container = new PostgreSQLContainer(DOCKER_IMAGE)
             .withUsername(USERNAME)
-            .withPassword(PASSWORD);
+            .withPassword(PASSWORD)
+            .withDatabaseName(DB_NAME);
 
-    static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
-
-        public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
-            TestPropertyValues.of(
-                    "spring.datasource.url=" + postgreSQLContainer.getJdbcUrl(),
-                    "spring.datasource.username=" + postgreSQLContainer.getUsername(),
-                    "spring.datasource.password=" + postgreSQLContainer.getPassword()
-            ).applyTo(configurableApplicationContext.getEnvironment());
-        }
+    @DynamicPropertySource
+    static void properties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", container::getJdbcUrl);
+        registry.add("spring.datasource.password", container::getPassword);
+        registry.add("spring.datasource.username", container::getUsername);
     }
 
     @Test
-    public void addCountryTest() {
-        Country country = new Country();
-        Assertions.assertNull(country);
+    @Transactional
+    void addCountryTest() {
+        Country country = countryService.addCountry("Ukraine");
+        assertEquals(country, countryRepository.findByCountry("Ukraine"));
     }
 
-    //    @Test
-    //    @Transactional
-    //    public void addCountryTest() {
-    //        String countryName = "UA";
-    //        countryService.addCountry(countryName);
-    //
-    //        Assertions.assertEquals(countryName, countryRepository.findByCountry("UA").getCountry());
-    //    }
+    @Test
+    @Transactional
+    void adTwoIdenticalCountryTest() {
+        countryService.addCountry("Ukraine");
+        Country country1 = countryService.addCountry("Ukraine");
+        assertNull(country1);
+    }
 
-    //    @Test
-    //    @Transactional
-    //    public void addTwoCountryWithIntentionalValuesTest() {
-    //        String countryName = "UA";
-    //        countryService.addCountry(countryName);
-    //        countryService.addCountry(countryName);
-    //                Assertions.assertEquals(new Country(2,"UA"),countryService.addCountry(countryName));
-    //    }
-    //
-    //    @Test
-    //    @Transactional
-    //    public void addCountryTestWithNull() {
-    //        Country country = countryService.addCountry(null);
-    //        Assertions.assertNull(country);
-    //    }
-    //
-    //    @Test
-    //    @Transactional
-    //    public void addAllCountry() {
-    //        countryService.addAllCountry();
-    //    }
-    //
-    //    @Test
-    //    @Transactional
-    //    public void getAllCountry() {
-    //        List<Country> countryList = countryService.getAllCountry();
-    //        Assertions.assertEquals(252, countryList.size());
-    //    }
-    //
-    //    @Test
-    //    @Transactional
-    //    public void getCountyByName() {
-    //        countryService.addCountry("UA");
-    //        Country country = new Country(1, "UA");
-    //        Assertions.assertEquals(country, countryService.getCountryByName("UA"));
-    //    }
-    //
-    //    @Test
-    //    @Transactional
-    //    public void getCountyByNameNull() {
-    //        Assertions.assertNull(countryService.getCountryByName("UA2"));
-    //    }
-    //
-    //    @Test
-    //    @Transactional
-    //    public void deleteCountryByName() {
-    //        countryService.deleteCountryByName("UA");
-    //        Assertions.assertNull(countryRepository.findByCountry("UA"));
-    //    }
+    @Test
+    @Transactional
+    public void addCountryWithEmptyName() {
+        assertNull(countryService.addCountry(""));
+    }
+
+    @Test
+    @Transactional
+    public void addCountryNameWithNumber() {
+        assertNull(countryService.addCountry("Ukraine1"));
+    }
+
+    @Test
+    @Transactional
+    public void addCountryNameShorterThanFive() {
+        assertNull(countryService.addCountry("UA"));
+    }
+
+    @Test
+    @Transactional
+    public void addAllCountry() {
+        countryService.addAllCountry();
+        assertEquals(252, countryService.getAllCountry().size());
+    }
+
+    @Test
+    @Transactional
+    public void getCountryByRightName() {
+        Country country = countryService.addCountry("Ukraine");
+        assertEquals(country, countryService.getCountryByName("Ukraine"));
+    }
+
+    @Test
+    @Transactional
+    public void getCountryByNullName() {
+        countryService.addCountry(null);
+        assertNull(countryService.getCountryByName("Ukraine"));
+    }
+
+    @Test
+    @Transactional
+    public void deleteCountryByRightName() {
+        countryService.getCountryByName("Ukraine");
+        countryService.deleteCountryByName("Ukraine");
+        assertNull(countryService.getCountryByName("Ukraine"));
+    }
+
 }
