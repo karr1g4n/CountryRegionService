4968c1d43fe6437c0a4609a02ff2d78f5e2767e6
Karr1g4n <licherep803@gmail.com>
2022-02-22 16:23:46 +0200

    created RegionService

---
+++ src/main/java/tech/pragmat/countryregionservice/service/RegionService.java
@@ -0,0 +1,54 @@
+package tech.pragmat.countryregionservice.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import tech.pragmat.countryregionservice.model.entity.Region;
+import tech.pragmat.countryregionservice.repository.RegionRepository;
+
+import java.util.List;
+
+@Service
+public class RegionService {
+
+    private final RegionRepository regionRepository;
+
+    @Autowired
+    public RegionService(RegionRepository regionRepository) {
+        this.regionRepository = regionRepository;
+    }
+
+    public Region addRegion(String name) {
+        if (validateRegion(name)) {
+            Region region = new Region();
+            region.setRegion(name);
+            return regionRepository.save(region);
+        } else {
+            return null;
+        }
+    }
+
+    public List<Region> getAllRegion() {
+        return regionRepository.findAll();
+    }
+
+    public Region getRegionByName(String name) {
+        return regionRepository.findByRegion(name);
+    }
+
+    public void deleteCountryByName(String name) {
+        regionRepository.deleteRegionByRegion(name);
+    }
+
+    private boolean validateRegion(String name) {
+        if (name != null && name.length() >= 5 && regionRepository.findByRegion(name) == null) {
+            for (int i = 0; i < name.length(); i++) {
+                if (Character.isDigit(name.charAt(i))) {
+                    return false;
+                }
+            }
+            return true;
+        }
+        return false;
+    }
+
+}
