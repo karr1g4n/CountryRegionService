c7874884a4dd774e916cbcb69fc1139c937a0fd3
Karr1g4n <licherep803@gmail.com>
2022-02-23 15:33:06 +0200

    added  @RequestMapping(value = "/country")

--- src/main/java/tech/pragmat/countryregionservice/web/controller/CountryController.java
+++ src/main/java/tech/pragmat/countryregionservice/web/controller/CountryController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import tech.pragmat.countryregionservice.model.entity.Country;
@@ -12,6 +13,7 @@ import tech.pragmat.countryregionservice.service.CountryService;
 import java.util.List;
 
 @RestController
+@RequestMapping(value = "/country")
 public class CountryController {
 
     private final CountryService countryService;
@@ -21,7 +23,7 @@ public class CountryController {
         this.countryService = countryService;
     }
 
-    @PostMapping()
+    @PostMapping
     public Country addCountryByName(@RequestParam String name) {
         return countryService.addCountry(name);
     }
@@ -41,7 +43,7 @@ public class CountryController {
         return countryService.getCountryByName(name);
     }
 
-    @DeleteMapping()
+    @DeleteMapping
     public void deleteCountryByName(@RequestParam String name){
         countryService.deleteCountryByName(name);
     }
