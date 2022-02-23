e11f0f6463a862b3ce6307df6ff5741989cf135d
Karr1g4n <licherep803@gmail.com>
2022-02-23 16:55:20 +0200

    fixed bug

--- src/main/java/tech/pragmat/countryregionservice/repository/NewCountryRegionRepository.java
+++ src/main/java/tech/pragmat/countryregionservice/repository/NewCountryRegionRepository.java
@@ -2,8 +2,9 @@ package tech.pragmat.countryregionservice.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import tech.pragmat.countryregionservice.model.entity.NewCountryRegion;
 
 @Repository
-public interface NewCountryRegionRepository extends JpaRepository<NewCountryRegionRepository, Integer> {
+public interface NewCountryRegionRepository extends JpaRepository<NewCountryRegion, Integer> {
 
 }
