0107f9524a329849cc5f7c5bd9e2ca29d11e3125
spring security <licherep803@gmail.com>
2022-03-03 00:11:29 +0900

    created new endpoints for NewCountryRegionController

--- src/main/java/tech/pragmat/countryregionservice/web/controller/NewCountryRegionController.java
+++ src/main/java/tech/pragmat/countryregionservice/web/controller/NewCountryRegionController.java
@@ -1,10 +1,12 @@
 package tech.pragmat.countryregionservice.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.*;
+import tech.pragmat.countryregionservice.model.entity.NewCountryRegion;
 import tech.pragmat.countryregionservice.service.NewCountryRegionService;
+
+import java.util.List;
 
 
 @RestController
@@ -18,10 +20,30 @@ public class NewCountryRegionController {
         this.countryRegionService = countryRegionService;
     }
 
-    @PostMapping("add/All/Country/Region")
-    public void addAllCountryRegion() {
+    @PostMapping()
+    public NewCountryRegion addNewCountryRegion(@RequestParam String countryName, String regionName) {
+        return countryRegionService.addNewCountryRegion(countryName, regionName);
+    }
+
+    @PostMapping("add/All/Countries/Regions")
+    public void addAllNewCountryRegion() {
         countryRegionService.addAll();
     }
 
+    @GetMapping("get/All/Countries/Regions")
+    public List<NewCountryRegion> getAllNewCountryRegion() {
+        return countryRegionService.getAllNewCountryRegion();
+    }
+
+    @GetMapping()
+    public NewCountryRegion getCountryRegion(@RequestParam String countryName) {
+        return countryRegionService.getNewCountryRegionByName(countryName);
+    }
+
+    @DeleteMapping()
+    @Transactional
+    public void DeleteNewCountryRegion(@RequestParam String countryName) {
+        countryRegionService.deleteNewCountryRegion(countryName);
+    }
 
 }
