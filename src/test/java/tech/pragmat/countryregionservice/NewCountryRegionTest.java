9a86f1e7e6cfa68627d3e063d72a3c40a68f6ed4
spring security <licherep803@gmail.com>
2022-03-02 21:21:49 +0900

    created Test for NewCountryRegionTest

--- src/test/java/tech/pragmat/countryregionservice/NewCountryRegionTest.java
+++ src/test/java/tech/pragmat/countryregionservice/NewCountryRegionTest.java
@@ -1,24 +1,43 @@
 package tech.pragmat.countryregionservice;
 
-import org.junit.Test;
+import org.junit.Before;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.transaction.annotation.Transactional;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import tech.pragmat.countryregionservice.model.entity.Country;
+import tech.pragmat.countryregionservice.model.entity.NewCountryRegion;
+import tech.pragmat.countryregionservice.model.entity.Region;
+import tech.pragmat.countryregionservice.repository.NewCountryRegionRepository;
+import tech.pragmat.countryregionservice.service.CountryService;
 import tech.pragmat.countryregionservice.service.NewCountryRegionService;
+import tech.pragmat.countryregionservice.service.RegionService;
 
 @SpringBootTest
 @Testcontainers
 public class NewCountryRegionTest {
 
     @Autowired
-    private NewCountryRegionService countryRegionService;
+    private CountryService countryService;
+
+    @Autowired
+    private RegionService regionService;
+
+    @Autowired
+    private NewCountryRegionService newCountryRegionService;
+
+    @Autowired
+    private NewCountryRegionRepository newCountryRegionRepository;
 
     private static final String DOCKER_IMAGE = "postgres:13";
-    private static final String DB_NAME = "region";
+    private static final String DB_NAME = "new_country_region";
     private static final String USERNAME = "postgres";
     private static final String PASSWORD = "43ahegib";
 
@@ -37,7 +56,47 @@ public class NewCountryRegionTest {
 
 
     @Test
-    public void addAll() {
-        countryRegionService.addAll();
+    @Transactional
+    public void AddNewCountryRegion() {
+        Country country = countryService.addCountry("Ukraine");
+        Region region = regionService.addRegion("EU");
+        NewCountryRegion newCountryRegion = newCountryRegionService.addNewCountryRegion("Ukraine", "EU");
+        Assertions.assertEquals(newCountryRegion, newCountryRegionRepository.findByCountry(country));
     }
+
+    @Test
+    @Transactional
+    public void addAllTest() {
+        newCountryRegionService.addAll();
+    }
+
+    @Test
+    @Transactional
+    public void GetAllNewCountryRegionTest() {
+        countryService.addAllCountry();
+        regionService.addAllRegion();
+        newCountryRegionService.addAll();
+        Assertions.assertEquals(252, newCountryRegionService.getAllNewCountryRegion().size());
+    }
+
+    @Test
+    @Transactional
+    public void GetNewCountryRegionByCountryTest() {
+        Country country = countryService.addCountry("Ukraine");
+        Region region = regionService.addRegion("EU");
+        NewCountryRegion newCountryRegion = newCountryRegionService.addNewCountryRegion("Ukraine", "EU");
+        Assertions.assertEquals(newCountryRegion, newCountryRegionService.getNewCountryRegion("Ukraine"));
+    }
+
+    @Test
+    @Transactional
+    public void deleteNewCountryRegionTest() {
+        Country country = countryService.addCountry("Ukraine");
+        Region region = regionService.addRegion("EU");
+        NewCountryRegion newCountryRegion = newCountryRegionService.addNewCountryRegion("Ukraine", "EU");
+        newCountryRegionService.deleteNewCountryRegion("Ukraine");
+        Assertions.assertNull(newCountryRegionService.getNewCountryRegion("Ukraine"));
+    }
+
+
 }
